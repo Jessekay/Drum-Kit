3bab8d256d6defbc4d2114b8567c13b569e0e514
JesseKay <kayjes00@gmail.com>
2025-06-24 14:46:08 +0200

    Find positions of all dots from 'a' to az'

--- codewars/connectDots.js
+++ codewars/connectDots.js
@@ -2,4 +2,14 @@ function connectTheDots(paper) {
     let grid = paper.split('\n').map(row => row.split(''));
     const height = grid.length;
     const width = grid[0].length;
+
+    let dots = {};
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const ch = grid[y][x];
+            if (ch >= 'a' && ch <= 'z') {
+                dots[ch] = {x, y};
+            }
+        }
+    }
 }
