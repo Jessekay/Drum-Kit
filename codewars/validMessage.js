f39ee8fb7bf8bd7fff5cf9bc0a95e0d1dceca36a
JesseKay <kayjes00@gmail.com>
2025-06-29 23:28:54 +0200

    extract the number

--- codewars/validMessage.js
+++ codewars/validMessage.js
@@ -1,3 +1,15 @@
 function isValidMessage(message) {
     if (message === "") return true;
 }
+
+  let i = 0;
+
+  while (i < message.length) {
+    let numStr = "";
+
+    // 🧮 Extract the number (could be more than one digit)
+    while (i < message.length && isDigit(message[i])) {
+      numStr += message[i];
+      i++;
+    }
+}
