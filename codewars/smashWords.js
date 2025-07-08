64e42a1fe2709d2db8d83aad7330e904ddf9e735
JesseKay <kayjes00@gmail.com>
2025-07-08 07:19:02 +0200

    executed a function that takes words and makes a sentence with no spaces at the beginning and the end

---
+++ codewars/smashWords.js
@@ -0,0 +1,14 @@
+function smash(words) {
+    let sentence = "";
+
+    for (let i = 0; i < words.length; i++) {
+        sentence += words[i];
+
+        if (i < words.length - 1) {
+            sentence += " ";
+        }
+    }
+    return sentence;
+}
+
+console.log(smash(['hello', 'world', 'this', 'is', 'great']));
