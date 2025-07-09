efc7c9327886955c3df0c7332996113f971d6d31
JesseKay <kayjes00@gmail.com>
2025-07-09 10:34:46 +0200

    implemented squareSum function using built in method reduce

--- squareSum.js
+++ squareSum.js
@@ -1,11 +1,5 @@
 function squareSum(numbers) {
-    let sum = 0;
-    
-    for (let i = 0; i< numbers.length; i++) {
-        sum += numbers[i] * numbers[i];
-    }
-
-    return sum;
+   return numbers.reduce((sum, num) => sum + num *num, 0);
 }
 
 console.log(squareSum([1, 2, 2])); // 9
