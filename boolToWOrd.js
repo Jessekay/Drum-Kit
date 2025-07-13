82afc4713fe2db8a35074f1bfaba7213c1821e8e
JesseKay <kayjes00@gmail.com>
2025-07-13 18:53:41 +0200

    converted boolean to string

---
+++ boolToWOrd.js
@@ -0,0 +1,11 @@
+function boolToWord(bool) {
+        if (bool == true) {
+            return "yes";
+        } else if (bool != true){
+            return "no";
+        }
+    
+    return bool;
+}
+
+console.log(boolToWord(true));
