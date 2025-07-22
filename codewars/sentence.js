7af3e1933171b4d44db78e8803eb7cc25c2ae189
JesseKay <kayjes00@gmail.com>
2025-07-22 21:17:38 +0200

    reversed a sentence using split method and normal looping

---
+++ codewars/sentence.js
@@ -0,0 +1,14 @@
+function sentenceReversing(sentence) {
+    const words = sentence.split(" ");
+    let reversedSentence = [];
+
+    for (let i = words.length; i >= 0; i--) {
+        reversedSentence.push(words[i]);
+    }
+    const reversed = reversedSentence.join("");
+    return reversed;
+}
+
+const sent = "I am a boy!";
+console.log(sentenceReversing(sent));
+ 
