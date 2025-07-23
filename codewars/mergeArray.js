da29ea7ad240afb730745bac7715919eb53c3ad1
JesseKay <kayjes00@gmail.com>
2025-07-23 06:06:06 +0200

    used a test case of a pair of empty arrays

--- codewars/mergeArray.js
+++ codewars/mergeArray.js
@@ -1,9 +1,11 @@
 function mergeAndSortUnique(arr1, arr2) {
-    const merged = [...arr1, ...arr2]
+    const merged = [...arr1, ...arr2];
+
+    const result = [...new Set(merged)].sort((a, b) => a - b);
+    return result;
+
 }
 
-const result = [...new Set(merged)].sort((a, b) => a - b);
 
-return result;
 
-console.log(mergeAndSortUnique([1,2,3,4], [5,6,7,8,9]));
+console.log(mergeAndSortUnique([], []));
